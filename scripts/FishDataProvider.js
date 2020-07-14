01f4e3e55935c73b1f4a6717182bef230953dadf
travisbrigman <travis1000@gmail.com>
2020-07-14 10:34:33 -0500

    added fish data provider

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,31 @@
+const fishCollection = [
+    {
+        name: "Bart",
+        food: "crustaceans"
+    },
+    {
+        name: "Dory",
+        food: "Plankton",
+        species: "Blue Tang",
+        length: 4,
+        location: "Great Barrier Reef",
+        image: ""
+    },
+    {
+        name: "Nemo",
+        food: "Plankton, Algae",
+        species: "Clown Fish",
+        length: 5,
+        location: "Great Barrier Reef",
+        image: ""
+    },
+    {
+        name: "Bruce",
+        food: "Fish, Tires",
+        species: "Great White Shark",
+        length: 25,
+        location: "Great Barrier Reef",
+        image: ""
+    },
+
+]
