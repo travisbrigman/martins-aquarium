db9d2db5422e986623f6772810554c7ea64a9646
travisbrigman <travis1000@gmail.com>
2020-07-15 11:06:41 -0500

    adding location files

---
+++ scripts/locations/LocationDataProvider.js
@@ -0,0 +1,39 @@
+const LocationDataCollection = [
+    {
+        image: "./images/xxx.jpeg",
+        alt: "",
+        name: "",
+        url: "",
+        linkText: "",
+        description: ""
+    },
+    {
+        image: "./images/xxx.jpeg",
+        alt: "",
+        name: "",
+        url: "",
+        linkText: "",
+        description: ""
+    },
+    {
+        image: "./images/xxx.jpeg",
+        alt: "",
+        name: "",
+        url: "",
+        linkText: "",
+        description: ""
+    },
+    {
+        image: "./images/xxx.jpeg",
+        alt: "",
+        name: "",
+        url: "",
+        linkText: "",
+        description: ""
+    },
+
+]
+
+export const useTip = () => {
+    return TipDataCollection.slice()
+}
